31084f484a266ecbd8badf60ef1f57281da44614
zains1234 <yusuf.zain.s@mail.ugm.ac.id>
2022-08-14 21:03:22 +0700

    feat(index): handling graphql connection and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./index.css";
+import App from "./App";
+import Browse from "./views/pages/Browse";
+import reportWebVitals from "./reportWebVitals";
+
+const client = new ApolloClient({
+  uri: 'https://graphql.anilist.co',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="" element={<Browse />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>        
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
