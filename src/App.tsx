4a5d6ca7337a99bb7d848affd5a39d759d82f933
zains1234 <yusuf.zain.s@mail.ugm.ac.id>
2022-08-17 21:31:29 +0700

    feat(App): add conditional error component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styled from "@emotion/styled";
 
 import Loading from "./views/components/Loading";
+import Error from "./views/pages/Error";
 import Navbar from "./views/Navbar";
 import "./App.css";
 
@@ -14,14 +15,19 @@ const MainContainer = styled.div({
 });
 
 function App() {
-  const [loadingState, updateLoadingState] = useState<boolean>(true);
+  const [loadingState, updateLoadingState] = useState<boolean>(false);
+  const [errorState, updateErrorState] = useState<boolean>(false);
 
   return (
     <div className="App">
       <Navbar />
       <MainContainer>
         {loadingState ? <Loading /> : ""}
-        <Outlet context={updateLoadingState} />
+        {errorState ? (
+          <Error code={500} />
+        ) : (
+          <Outlet context={[updateLoadingState, updateErrorState]} />
+        )}
       </MainContainer>
     </div>
   );
