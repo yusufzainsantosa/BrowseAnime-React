856d51298e6d549b05d77398f0d70dc83b889430
zains1234 <yusuf.zain.s@mail.ugm.ac.id>
2022-08-17 19:27:10 +0700

    feat(Type): change animeinfo to animedetail and add new data type

--- src/interfaces/Type.tsx
+++ src/interfaces/Type.tsx
@@ -1,4 +1,87 @@
 type optionalNumber = number | null;
+type optionalString = string | null;
+
+export interface AnimeDetail {
+  id: number;
+  title: {
+    userPreferred: string;
+    romaji: string;
+  };
+  coverImage: {
+    color: string;
+    large: string;
+  };
+  bannerImage: optionalString;
+  startDate: {
+    year: optionalNumber;
+    month: optionalNumber;
+    day: optionalNumber;
+  };
+  endDate: {
+    year: optionalNumber;
+    month: optionalNumber;
+    day: optionalNumber;
+  };
+  description: string;
+  season: optionalString;
+  seasonYear: optionalNumber;
+  type: string;
+  format: optionalString;
+  status: optionalString;
+  episodes: optionalString;
+  duration: optionalNumber;
+  chapters: optionalNumber;
+  volumes: optionalNumber;
+  genres: string[];
+  synonyms: string[];
+  source: string;
+  isAdult: boolean;
+  isLocked: boolean;
+  meanScore: number;
+  averageScore: number;
+  popularity: number;
+  favourites: number;
+  isFavouriteBlocked: boolean;
+  hashtag: optionalString;
+  countryOfOrigin: string;
+  isLicensed: boolean;
+  isFavourite: boolean;
+  isRecommendationBlocked: boolean;
+  isReviewBlocked: boolean;
+  nextAiringEpisode: {
+    airingAt: optionalNumber;
+    timeUntilAiring: optionalNumber;
+    episode: optionalNumber;
+  } | null;
+}
+
+export interface Character {
+  id: number;
+  name: {
+    userPreferred: string;
+  };
+  image: {
+    medium: string;
+  };
+}
+
+export interface Staff extends Character {
+  language: string;
+}
+
+export interface AnimeCharacters {
+  id: number;
+  role: optionalString;
+  name: optionalString;
+  voiceActors: Staff[];
+  node: Character;
+}
+
+export interface AnimeStaff {
+  id: number;
+  role: string;
+  node: Staff;
+}
 
 export interface AnimeInfo {
   id: number;
@@ -7,17 +90,11 @@ export interface AnimeInfo {
     userPreferred: string;
   };
   genres: string[];
+  format: optionalString;
+  episodes: optionalString;
   coverImage: {
     color: string;
     large: string;
-  };
-  status: string;
-  season: string | null;
-  seasonYear: optionalNumber;
-  endDate: {
-    year: optionalNumber;
-    month: optionalNumber;
-    day: optionalNumber;
   };
 }
 
@@ -27,4 +104,10 @@ export interface PageInfo {
   currentPage: number;
   lastPage: number;
   hasNextPage: boolean;
+}
+
+export interface QueryResponse {
+  loading: boolean;
+  error?: any;
+  refetch?: any;
 }
